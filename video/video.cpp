fe8404c13a5794ae81d336efb7dddfbda4e3e6be
saber <saber@localhost.localdomain>
2015-12-04 20:32:47 +0800

    ‘test

--- video/video.cpp
+++ video/video.cpp
@@ -1,20 +1,20 @@
 
-###
 #include"video.h"
-#include<iostream>
-using namespace::std;
+AVFormatContext *pFormatCtx;
+AVCodecContext  *pCodecCtx;
+AVFrame         *pFrame, *pFrameBGR;
+uint8_t         *bufferBGR;
 //SwsContext      *pConvertCtx;
 //IplImage *img;
 //VideoWriter writer("/home/saber/模板/raspi/video/VideoTest.avi", CV_FOURCC('M', 'J', 'P', 'G'), 25.0, Size(360, 480));
-int Video::init_video(void){
-  return 0;
-}
-int Video:: get_video(void)
+int init_video(void)
 {
   return 0;
 }
-Video::Video(void){
+int get_video(void)
+{
+  return 0;
 }
-Video::~Video(void){
-}
+
+
 
